00680938a50d63422b13377513b18d00f30bbd4a
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-02-22 10:30:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ with col1:
 
 with col2: 
   st.subheader("Esta es la segunda columna")
-  modo = st.radio("Que modalidad es la principal en tu interfaz", ("Visual", "auditiva", "Táctil"))
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ("Visual", "Auditiva", "Táctil"))
   if modo == "Visual":
     st.write("La vista es fundamental para tu interfaz")
-  if modo = "auditiva":
+  if modo == "Auditiva":
     st-write("La audición es fundamental para tu interfaz")
-  if modo = "Táctil";
+  if modo == "Táctil";
     st.write("Lo táctil es fundamental para tu interfaz")
 
