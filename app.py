6c39a26b6269a209043bf23541023e3d87296f9c
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-02-22 10:07:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 from PIL import Image
 
 st.title("Mi primera App")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+image = Image.open("roma.jpg")
