49875631dd0d1f24561cad5be6c9bfca325117aa
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-02-22 10:21:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ st.image(image, caption="Roma")
 
 texto = st.text_input("Escribe algo", "Este es mi texto")
 st.write("El texto escrito es", texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+
+col1, col2 = st.columns(2)
